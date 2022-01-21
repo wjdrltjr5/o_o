b7db262eb589a4f113efa334f0fc1fb53540880b
Jeong gi seok <wjdrltjr5@naver.com>
2022-01-21 17:49:06 +0900

    section store

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -51,3 +51,61 @@ new Swiper(".notice-line .swiper", {
 	autoplay: true,
 	loop: true,
 });
+
+new Swiper(".promotion .swiper", {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	centeredSlides: true,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+	},
+	pagination: {
+		el: ".promotion .swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		prevEl: ".promotion .swiper-prev",
+		nextEl: ".promotion .swiper-next",
+	},
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+	isHidePromotion = !isHidePromotion;
+	if (isHidePromotion) {
+		promotionEl.classList.add("hide");
+	} else {
+		promotionEl.classList.remove("hide");
+	}
+});
+
+function random(min, max) {
+	return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+	// gsap.to(요소,시간,옵션)
+	gsap.to(selector, random(1.5, 2.5), {
+		y: size,
+		repeat: -1,
+		yoyo: true,
+		ease: Power1.easeInout,
+		delay: random(0, delay),
+	});
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+	new ScrollMagin.Scene({
+		triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+		tirggerHook: 0.8,
+	})
+		.setClassToggle()
+		.addTo();
+});
